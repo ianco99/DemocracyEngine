559ed7c455fa97889949dc9b951238a284017302
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-08-13 12:11:04 -0300

    Hello triangle

--- DemocracyEngine/DemocracyEngine/src/Renderer.cpp
+++ DemocracyEngine/DemocracyEngine/src/Renderer.cpp
@@ -18,5 +18,11 @@ namespace DemoEngine_Renderer
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 	}
 }
