f260539de36e34f7d3dc5f8d13881bf0c6de5cde
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-08-13 10:11:24 -0300

    Implemented Window class

--- DemocracyEngine/DemocracyEngine/src/main.cpp
+++ DemocracyEngine/DemocracyEngine/src/main.cpp
@@ -1,32 +1,26 @@
 #include "glfw3.h"
+#include "Window.h"
+
+using namespace DemoEngine_Window;
 
 int main(void)
 {
-    GLFWwindow* window;
+    //GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    
+    Window window = Window(1024, 720, "Democracy Engine");
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window.GetGLFWwindow()))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window.GetGLFWwindow());
 
         /* Poll for and process events */
         glfwPollEvents();
