a278118cf768704d6c730ff63f5e295c4828e4f6
BiscupidTea <109319881+BiscupidTea@users.noreply.github.com>
2024-09-10 12:07:32 -0300

    Fixed Triangle Creation

--- DemocracyEngine/DemocracyEngine/src/Entities/Triangle.cpp
+++ DemocracyEngine/DemocracyEngine/src/Entities/Triangle.cpp
@@ -7,14 +7,14 @@ namespace DemoEngine_Entities
 		int vertexSize = 9;
 		float vertex[] = {
 			//position              color
+			 0.0f,  0.5f, 0.0f,		//0.0f, 0.0f, 1.0f  //top
 			-0.5f, -0.5f, 0.0f,		//1.0f, 0.0f, 0.0f, //botton right
 			 0.5f, -0.5f, 0.0f,		//0.0f, 1.0f, 0.0f, //botton left
-			 0.0f,  0.5f, 0.0f,		//0.0f, 0.0f, 1.0f  //top
 		};
 
 		int indexSize = 3;
 		int indices[] = {
-		0, 1, 3
+		0, 1, 2
 		};
 
 		Renderer::GetRender()->CreateShape(VBO, VAO, EBO, vertex, indices, vertexSize, indexSize);
