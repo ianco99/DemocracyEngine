688e892f85fa18714069d4cf8a2f71dbc92e364d
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2024-11-11 14:49:08 -0300

    Fix Sprite Draw

--- DemocracyEngine/DemocracyEngine/src/Entities/Sprite.cpp
+++ DemocracyEngine/DemocracyEngine/src/Entities/Sprite.cpp
@@ -11,10 +11,10 @@ namespace DemoEngine_Entities
 		vertexSize = 36;
 		float vertex[] = {
 			// positions		    // colors					// texture coords
-			0.5f, 0.5f, 0.0f,       1.0f, 1.0f, 1.0f, 0.0f,     1.0f, 1.0f,   // top right
-			0.5f, -0.5f, 0.0f,      1.0f, 1.0f, 1.0f, 0.0f,	    1.0f, 0.0f,   // bottom right
-			-0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f, 0.0f,	    0.0f, 0.0f,   // bottom left
-			-0.5f, 0.5f, 0.0f,      1.0f, 1.0f, 1.0f, 0.0f,	    0.0f, 1.0f    // top left 
+			0.5f, 0.5f, 0.0f,       1.0f, 1.0f, 1.0f, 1.0f,     1.0f, 1.0f,   // top right
+			0.5f, -0.5f, 0.0f,      1.0f, 1.0f, 1.0f, 1.0f,	    1.0f, 0.0f,   // bottom right
+			-0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f, 1.0f,	    0.0f, 0.0f,   // bottom left
+			-0.5f, 0.5f, 0.0f,      1.0f, 1.0f, 1.0f, 1.0f,	    0.0f, 1.0f    // top left 
 		};
 
 		indexSize = 6;
